e2679d8a786c5219bd4c00494e11c72916d5a030
Khoa Bui <thomasbui93@gmail.com>
2018-03-02 00:45:20 +0200

    add mocha and typescript testing

--- webapp/app/server.ts
+++ webapp/app/server.ts
@@ -1,5 +1,5 @@
 'use strict';
-import express from 'express';
+import * as express from 'express';
 
 // Constants
 const PORT = 8000;
@@ -7,9 +7,6 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world! this is nice');
-});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`)
