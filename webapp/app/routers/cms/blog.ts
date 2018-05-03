a97a0ec82e4322e6b8ef9dea10d25608bcff712d
Khoa Bui <thomasbui93@gmail.com>
2018-05-03 20:03:37 +0100

    containerize backend code

--- webapp/app/routers/cms/blog.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.json({ blog: "sample blog" });
-  } catch (err) {
-    next(err);
-  }
-};
--- webapp/app/shell.ts
+++
@@ -1,7 +0,0 @@
-import shelljs from "shelljs";
-
-if ( process.env.NODE_ENV === "development") {
-  shelljs.exec("npm run dev");
-} else {
-  shelljs.exec("npm start");
-}
